b53d976da73ac175ffce1e6b374424928de5af12
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-07 00:37:08 +0700

    adding setInterval and setTimeout function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const playerPick = document.querySelectorAll("#player-area img");
 const resultDisplay = document.getElementById("result-area");
 const refresh = document.querySelector(".new-game");
+const compArea = document.querySelectorAll("#comp-area div");
+const playerArea = document.querySelectorAll("#player-area div");
 
 function getComPick() {
   const com = Math.trunc(Math.random() * 3) + 1;
@@ -16,6 +18,19 @@ const getResult = function (comPick, playerPick) {
   if (playerPick === "gunting") return comPick === "kertas" ? "PLAYER 1 <br> WIN" : "COM <br> WIN";
 };
 
+function loadingPick() {
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(function () {
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval;
+      return;
+    }
+    compArea[i++].classList.toggle("hidden");
+    if (i === compArea.length) i = 0;
+  }, 100);
+}
+
 let playing = true;
 
 playerPick.forEach(function (i) {
@@ -27,36 +42,44 @@ playerPick.forEach(function (i) {
       const playerHighlight = document.querySelector(`.btn--${playerPick}`);
       playerHighlight.classList.add("hidden");
 
-      // Computer pick choice
-      const comPick = getComPick();
-      console.log(`Computer memilih : ${comPick}`);
-      const comHighlight = document.querySelector(`.comp--${comPick}`);
-      comHighlight.classList.add("hidden");
+      loadingPick();
 
-      // Showing the result
-      const hasil = getResult(comPick, playerPick);
-      console.log(`Hasilnya : ${hasil}`);
-      resultDisplay.innerHTML = hasil;
-      if (hasil === "DRAW") {
-        resultDisplay.classList.add("draw");
-      } else {
-        resultDisplay.classList.add("hasil");
-      }
+      setTimeout(function () {
+        for (let i = 0; i < compArea.length; i++) {
+          compArea[i].classList.remove("hidden");
+        }
 
-      // Player 1 cannot pick again unless its refreshed
-      playing = false;
+        // Computer pick choice
+        const comPick = getComPick();
+        console.log(`Computer memilih : ${comPick}`);
+        const comHighlight = document.querySelector(`.comp--${comPick}`);
+        comHighlight.classList.add("hidden");
+
+        // Showing the result
+        const hasil = getResult(comPick, playerPick);
+        console.log(`Hasilnya : ${hasil}`);
+        resultDisplay.innerHTML = hasil;
+        if (hasil === "DRAW") {
+          resultDisplay.classList.add("draw");
+        } else {
+          resultDisplay.classList.add("hasil");
+        }
+
+        // Player 1 cannot pick again unless its refreshed
+        playing = false;
+      }, 1000);
     }
   });
 });
 
 refresh.addEventListener("click", function () {
   playing = true;
-  document.querySelector(`.btn--batu`).classList.remove("hidden");
-  document.querySelector(".btn--kertas").classList.remove("hidden");
-  document.querySelector(".btn--gunting").classList.remove("hidden");
-  document.querySelector(".comp--batu").classList.remove("hidden");
-  document.querySelector(".comp--kertas").classList.remove("hidden");
-  document.querySelector(".comp--gunting").classList.remove("hidden");
+  for (let i = 0; i < compArea.length; i++) {
+    compArea[i].classList.remove("hidden");
+  }
+  for (let i = 0; i < playerArea.length; i++) {
+    playerArea[i].classList.remove("hidden");
+  }
   resultDisplay.classList.remove("draw");
   resultDisplay.classList.remove("hasil");
   resultDisplay.innerHTML = "<h1>VS</h1>";
