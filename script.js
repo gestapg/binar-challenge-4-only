e46b9cdd5c39d7780e697316d34113f17d9e678b
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-07 21:24:29 +0700

    menambahkan OOP

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ const refreshBtn = document.querySelector(".new-game");
 const resultDisplay = document.getElementById("result-area");
 const compArea = document.querySelectorAll("#comp-area div");
 const playerArea = document.querySelectorAll("#player-area div");
+class Game {
+  static getComPick() {
+    const com = Math.trunc(Math.random() * 3) + 1;
+    if (com === 1) return "batu";
+    if (com === 2) return "kertas";
+    return "gunting";
+  }
+  static getResult(comPick, playerPick) {
+    if (playerPick === comPick) return "DRAW";
+    if (playerPick === "batu") return comPick === "gunting" ? "PLAYER 1 <br> WIN" : "COM <br> WIN";
+    if (playerPick === "kertas") return comPick === "batu" ? "PLAYER 1 <br> WIN" : "COM <br> WIN";
+    if (playerPick === "gunting") return comPick === "kertas" ? "PLAYER 1 <br> WIN" : "COM <br> WIN";
+  }
 
-function getComPick() {
-  const com = Math.trunc(Math.random() * 3) + 1;
-  if (com === 1) return "batu";
-  if (com === 2) return "kertas";
-  return "gunting";
-}
-
-const getResult = function (comPick, playerPick) {
-  if (playerPick === comPick) return "DRAW";
-  if (playerPick === "batu") return comPick === "gunting" ? "PLAYER 1 <br> WIN" : "COM <br> WIN";
-  if (playerPick === "kertas") return comPick === "batu" ? "PLAYER 1 <br> WIN" : "COM <br> WIN";
-  if (playerPick === "gunting") return comPick === "kertas" ? "PLAYER 1 <br> WIN" : "COM <br> WIN";
-};
-
-function loadingPick() {
-  let i = 0;
-  const waktuMulai = new Date().getTime();
-  setInterval(function () {
-    if (new Date().getTime() - waktuMulai > 1000) {
-      clearInterval;
-      return;
-    }
-    compArea[i++].classList.toggle("hidden");
-    if (i === compArea.length) i = 0;
-  }, 100);
+  static loadingPick() {
+    let i = 0;
+    const waktuMulai = new Date().getTime();
+    setInterval(function () {
+      if (new Date().getTime() - waktuMulai > 1000) {
+        clearInterval;
+        return;
+      }
+      compArea[i++].classList.toggle("hidden");
+      if (i === compArea.length) i = 0;
+    }, 100);
+  }
 }
 
 let playing = true;
@@ -46,7 +46,7 @@ playerPickBtn.forEach(function (i) {
       playing = false;
 
       // Waiting for the computer pick
-      loadingPick();
+      Game.loadingPick();
 
       // After 1 second
       setTimeout(function () {
@@ -55,13 +55,13 @@ playerPickBtn.forEach(function (i) {
         }
 
         // Computer pick choice
-        const comPick = getComPick();
+        const comPick = Game.getComPick();
         console.log(`Computer memilih : ${comPick}`);
         const comHighlight = document.querySelector(`.comp--${comPick}`);
         comHighlight.classList.add("hidden");
 
         // Showing the result
-        const hasil = getResult(comPick, playerPick);
+        const hasil = Game.getResult(comPick, playerPick);
         console.log(`Hasilnya : ${hasil}`);
         resultDisplay.innerHTML = hasil;
         if (hasil === "DRAW") {
